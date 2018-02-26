a5e6971b4cd5e8152befcdef7cd787d5e17828f7
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-26 19:39:07 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 class Artist
   attr_accessor :name, :genre, :songs
-  @@song_count=[]
+  @@all_songs=[]
+  @@song_count
   def initialize(name)
     @name=name
     @genre
@@ -9,17 +10,18 @@ class Artist
   def add_song(song)
     song.artist = self
     @songs << song
-    @@song_count << song
+    @@all_songs << song
   end
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     song.artist = self
     @songs << song
-    @@song_count << song
+    @@all_songs << song
   end
 
   def self.song_count
-    @@song_count.size
+    @@song_count = @@all_songs.size
+    @@song_count
   end
 
 end

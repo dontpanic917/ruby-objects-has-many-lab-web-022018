43d6a46d4fd1a3ad016a96e5415b3103e70ad318
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-26 19:28:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,8 +6,9 @@ class Artist
     @songs=[]
   end
   def add_song(song)
-    song.artist = self
+    
     @songs << song
+    song.artist = self
   end
   def add_song_by_name(song_name)
     song = Song.new(song_name)

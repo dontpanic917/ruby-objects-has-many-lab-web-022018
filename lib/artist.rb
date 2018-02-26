fc661779072a2a510f8f5caae75c5fb3eb646a5f
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-26 19:39:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,21 @@
 class Artist
   attr_accessor :name, :genre, :songs
+  
   @@all_songs=[]
   @@song_count
+
   def initialize(name)
     @name=name
     @genre
     @songs=[]
   end
+
   def add_song(song)
     song.artist = self
     @songs << song
     @@all_songs << song
   end
+
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     song.artist = self
